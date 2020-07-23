73201e9df9dc379b120a1bb55313ad83f9e3e743
Scott Richmond <s.richmond@utoronto.ca>
2020-07-23 17:10:09 -0400

    Move lots of things to cutting_room

--- cutting_room/proto.js
+++ cutting_room/proto.js
@@ -1,17 +1,22 @@
 // prototype-based polymorphism
-// probably I'll need an inheritance chain that is separate from the JS mess
-// the idea is for multimethods to install themselves on this prototype chain
-// that way I'm not munging with the weird JS inheritance stuff (is [] instanceof Array? And instanceof Object is kind of a mess. And so are the atomic types.)
-// (I may revise this if it turns out performance is an issue.)
-// The idea is that multimethods install a method on the prototype, and dispatch using the prototype chain.
-// I need a function that calls one of these methods (think `$call`). Basically, I need a functional abstraction over method calls. (I can even avoid `this` here, since every function will be stateless. That said, the API will be such that the instance of the datatype it's called on is passed as the *first* argument to the pure function.)
-// I think this is enough to get multimethods using prototypes in addition to/in place of functional tests. (Functional tests, as currently already exist in multi.js, should be optional!--since they allow for simpler, if slower, dispatch, that responds to the shape of the data rather than its type.)
+// uses & mungs constructors to convert to a parallel inheritance chain
 // This will mean getting careful about inheritance, figuring out if there's a plausible single-chain inheritance diagram for the types I need, etc.
 // This will also likely mean setting up an additional system of defining new types.
+// Both of these are already done, although what's here is only the bare bones.
+// in other words, this is a proof of concept, not a fully worked out inheritance hierarchy
 // That said, the dispatch system, as long as it presumes an abstraction to be followed elsewhere, will be able to be separate from designing the concrete inheritance diagram.
 // And *that* said, if it is possible, it is worth being sure that the Ludus system for declaring and implementing types/dispatch is robust enough that it can be what I use to build out the inheritance hierarchy in the first place.
+// Also: multimethods should, as in clojure, allow functional testing for dispatch, as is currently outlined in poly.js. (In other words, `multi` should be variadic, using n_ary in fn.js.)
+// Finally, there are speed issues here. I think it's not necessary to worry about them (yet), but it may be necessary.
 
-// this is a proof of concept, not a fully worked out inheritance hierarchy
+///BUT BUT BUT
+// Clojure's multimethods don't actually do the whole inheritance chain thing: the lookup is functional. So the functional dispatch _is_ the core of the multimethod.
+// AND, type is actually super easy to write: `x => x === null ? null : x.constructor;` (since null can be a key in a `Map`).
+// That ensures that you can dispatch super easily on type, but not worry about
+// inheritance problems.
+// There is still the question of creating new types, but that itself
+// can be a fully separate problem from dispatch.
+
 
 let derive = (obj_name, proto) => globalThis.Object.create(proto, {[name]: {value: obj_name}});
 let create = (the_type, obj) => globalThis.Object.assign(globalThis.Object.create({constructor: {[type]: the_type}}), obj);
