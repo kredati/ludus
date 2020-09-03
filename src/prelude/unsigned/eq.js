114ff1cc3ae8f966ba34a2b7242d5c29a233e2c6
Scott Richmond <s.richmond@utoronto.ca>
2020-09-03 16:19:04 -0400

    Unsigned base `eq` algorithm for `hash`

--- src/prelude/unsigned/eq.js
+++ src/prelude/unsigned/eq.js
@@ -1,49 +1,12 @@
-import {defn, partial} from './functions.js';
 import {is_sequence, is_assoc, is_some} from './preds.js';
 
-let is_identical = defn({
-  name: 'is_identical',
-  doc: 'Returns true if two values are reference-identical (JS ===). This is a much faster operation than `eq`, but may give unintuitive answers.',
-  body: [
-    (x) => partial(is_identical, x),
-    (x, y) => x === y,
-    (x, y, z, ...rest) => {
-      let as = [y, z, ...rest];
-      for (let a of as) {
-        if (!is_identical(x, a)) return false;
-      }
-      return true;
-    } 
-  ]
-});
-
-let eq = defn({
-  name: 'eq',
-  doc: `Are two things the same? This function tries to answer that question.
-  
-  As you might guess, this algorithm is rather more complex that it might at first sound, and involves assumptions about the nature of things. For the most part, \`eq\` will give you the answers that you expect. 
-  
-  For atomic values (string, boolean, number, etc.), it's simple and returns quickly.
-  
-  For collections, it tries to a few tricks to be fast. When those fail: for associative collections, it tests for equality at each key; for sequential collections, it tests for equality in order. Testing the equality of large collections should be done sparingly, since it may be quite slow. If you are testing for changes in a collection you are working with, you may wish to use \`is_identical\` instead.`,
-  body: [
-    (x) => partial(eq, x),
-    (x, y) => {
-      if (x === y) return true;
-      if (x == undefined) return y == undefined;
-      if (is_sequence(x)) return eq_iter(x, y);
-      if (is_assoc(x)) return eq_assoc(x, y);
-      return false;
-    },
-    (x, y, z, ...rest) => {
-      let as = [y, z, ...rest];
-      for (let a of as) {
-        if (!eq(x, a)) return false;
-      }
-      return true;
-    }
-  ]
-});
+let eq = (x, y) => {
+  if (x === y) return true;
+  if (x == undefined) return y == undefined;
+  if (is_sequence(x)) return eq_iter(x, y);
+  if (is_assoc(x)) return eq_assoc(x, y);
+  return false;
+};
 
 // eq_iter is a helper function for `eq`
 // it tests if two iterables are equal(ish), using `eq`
@@ -90,4 +53,4 @@ let eq_assoc = (x, y) => {
   return true;
 };
 
-export {is_identical, eq};
+export {eq};
