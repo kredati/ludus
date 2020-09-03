61010cf9086aee8ff148e13741b9c401965b2ec7
Scott Richmond <s.richmond@utoronto.ca>
2020-09-03 16:24:04 -0400

    Unsign all preds

--- src/prelude/unsigned/preds.js
+++ src/prelude/unsigned/preds.js
@@ -3,100 +3,38 @@
 // information about JS values to functions.
 // Equality, however, is handled in './eq.js'.
 
-import {defn} from './functions.js';
+let bool = (x) => x !== false && x != undefined;
 
-let bool = defn({
-  name: 'bool',
-  doc: 'Coerces a value to boolean `true` or `false`: returns false if a value is `false` or `undefined`. Otherwise returns true.',
-  body: (x) => x !== false && x != undefined
-});
+let is_any = (_) => true;
 
-let is_any = defn({
-  name: 'is_any',
-  doc: 'Always returns true: could be anything.',
-  body: (_) => true
-});
+let is_undef = (x) => x == undefined;
 
-let is_undef = defn({
-  name: 'is_undef',
-  doc: 'Returns true if the passed value is undefined.',
-  body: (x) => x == undefined
-});
+let is_some = (x) => x != undefined;
 
-let is_some = defn({
-  name: 'is_some',
-  doc: 'Returns true if the passed value is *not* undefined.',
-  body: (x) => x != undefined
-});
+let is_string = (x) => typeof x === 'string';
 
-let is_string = defn({
-  name: 'is_string',
-  doc: 'Returns true if the passed value is a string.',
-  body: (x) => typeof x === 'string'
-});
+let is_number = (x) => typeof x === 'number';
 
-let is_number = defn({
-  name: 'is_number',
-  doc: 'Returns true if the passed value is a number.',
-  body: (x) => typeof x === 'number'
-});
+let is_int = (x) => is_number(x) && x % 1 === 0;
 
-let is_int = defn({
-  name: 'is_int',
-  doc: 'Returns true if the value passed is an integer.',
-  body: (x) => is_number(x) && x % 1 === 0
-});
+let is_bigint = (x) => typeof x === 'bigint';
 
-let is_bigint = defn({
-  name: 'is_bigint',
-  doc: 'Returns true if the passed value is a bigint.',
-  body: (x) => typeof x === 'bigint'
-});
+let is_bool = (x) => typeof x === 'boolean';
 
-let is_bool = defn({
-  name: 'is_bool',
-  doc: 'Returns true if the value passed is a boolean.',
-  body: (x) => typeof x === 'boolean'
-});
+let is_symbol = (x) => typeof x === 'symbol';
 
-let is_symbol = defn({
-  name: 'is_symbol',
-  doc: 'Returns true if the value passed is a symbol.',
-  body: (x) => typeof x === 'symbol'
-});
+let is_fn = (x) => typeof x === 'function';
 
-let is_fn = defn({
-  name: 'is_fn',
-  doc: 'Returns true if the value passed is a function.',
-  body: (x) => typeof x === 'function'
-});
+let is_obj = (x) => x !== null && typeof x === 'object';
 
-let is_obj = defn({
-  name: 'is_obj',
-  doc: 'Returns true if the value passed is an object. NB: All collections are objects: object literals, but also arrays, vectors, lists, etc.',
-  body: (x) => x !== null && typeof x === 'object'
-});
+let is_assoc = (x) => x != undefined && x.constructor === Object;
 
-let is_assoc = defn({
-  name: 'is_assoc',
-  doc: 'Returns true if the value passed in is an associative object, holding values at string keys.',
-  body: (x) => x != undefined && x.constructor === Object
-});
+let is_iter = (x) => x != undefined && typeof x[Symbol.iterator] === 'function';
 
-let is_iter = defn({
-  name: 'is_iter',
-  doc: 'Returns true if the value passed in conforms to the JS iterable protocol.',
-  body: (x) => x != undefined && typeof x[Symbol.iterator] === 'function'
-});
-
-let is_sequence = defn({
-  name: 'is_sequence',
-  doc: 'Returns true if the value passed in is a sequence: an iterable collection. (Strings are iterable, but they are not sequences.)',
-  body: (x) => 
-    x != undefined 
-    && typeof x !== 'string' 
-    && typeof x[Symbol.iterator] === 'function'
-});
+let is_sequence = (x) => 
+  x != undefined 
+  && typeof x !== 'string' 
+  && typeof x[Symbol.iterator] === 'function';
 
 export {
   bool, is_any, is_undef, is_some, 
