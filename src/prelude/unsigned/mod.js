6275213e9755045def28b4e246161f6de30df318
Scott Richmond <s.richmond@utoronto.ca>
2020-10-11 14:00:24 -0400

    Fix exports; keep things in namespaces for now

--- src/prelude/unsigned/mod.js
+++ src/prelude/unsigned/mod.js
@@ -1,33 +1,12 @@
-import Ludus from './env.js';
-import * as errors from './errors.js';
-import * as fns from './fns.js';
-import * as eq from './eq.js';
-import * as preds from './preds.js';
-import * as spec from './spec.js';
-import * as util from './util.js';
+import Ludus from './base.js';
+import {eq} from './eq.js';
+import Error from './errors.js';
+import Fn from './fns.js';
+import Pred from './preds.js';
+import Spec from './spec.js';
 
-let has = (key, obj) => Object.prototype.hasOwnProperty.call(obj, key);
-
-let merge = fns.defn({
-  name: 'merge',
-  doc: 'Merges objects: creates a new object with properties from all the passed in objects combined ("enumerable own properties", in JS-speak). Throws an error if any of the objects have keys duplicated between them.',
-  pre: spec.splat(preds.is_obj),
-  body: (...objs) => {
-    let out = {};
-    for (let obj of objs) {
-      for (let key in obj) {
-        if (!has(key, obj)) continue;
-        if (key in out) throw Error(`Name conflict: ${key} is already in ${out}`);
-        out[key] = obj[key];
-      }
-    }
-    return out;
-  }
+Ludus.NS.defmembers(Ludus, {
+  Error, Fn, Pred, Spec, eq
 });
 
-let Prelude = Ludus.ns({
-  name: 'Prelude', 
-  space: merge(errors, fns, eq, spec, preds, util, {merge}, Ludus[Symbol.for('ludus/ns/space')])
-});
-
-export default Prelude;
+export default Ludus;
