d5dd29f0dec035b79af4bd5b7f3bb1c454b79e40
Scott Richmond <s.richmond@utoronto.ca>
2020-10-15 19:18:41 -0400

    Fix imports & exports

--- src/prelude/signed/eq.js
+++ src/prelude/signed/eq.js
@@ -3,11 +3,10 @@
 // but also metaphysically.
 
 import L from './deps.js';
-import F from './fns.js';
-import P from './preds.js';
+import './fns.js';
 
-let {defn, partial} = F;
-let {is_sequence, is_assoc, is_some} = P;
+let eq_ = L.eq;
+let {defn, partial} = L.Fn;
 
 let is_identical = defn({
   name: 'is_identical',
@@ -36,7 +35,7 @@ let eq = defn({
   For collections, it tries to a few tricks to be fast. When those fail: for associative collections, it tests for equality at each key; for sequential collections, it tests for equality in order. Testing the equality of large and/or nested collections should be done sparingly, since it may be quite slow. If you are testing for changes in an immutable collection (e.g. arrays) you are working with, you may wish to use \`is_identical\` instead.`,
   body: [
     (x) => partial(eq, x),
-    (x, y) => L.eq(x, y),
+    (x, y) => eq_(x, y),
     (x, y, z, ...rest) => {
       let as = [y, z, ...rest];
       for (let a of as) {
@@ -47,12 +46,4 @@ let eq = defn({
   ]
 });
 
-let Eq = L.ns({
-  name: 'Eq',
-  space: {
-    is_identical,
-    eq
-  }
-})
-
-export default Eq;
+export default L.NS.defmembers(L, {eq, is_identical});
