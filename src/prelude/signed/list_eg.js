f53459e809675305aef8aa40ae4a87bad9697b97
Scott Richmond <s.richmond@utoronto.ca>
2020-10-15 19:21:24 -0400

    Fix imports

--- src/prelude/signed/list_eg.js
+++ src/prelude/signed/list_eg.js
@@ -1,4 +1,6 @@
-import {Type, NS} from './base.js';
+import Ludus from './deps.js';
+
+let {NS, Type} = Ludus;
 
 let {create, deftype} = Type;
 
@@ -32,3 +34,5 @@ let is_empty = (x) => x === empty;
 export default NS.defns({name: 'List', type: List, members: {
   List, empty, cons, first, rest, iterate, conj, list, show, is_list, is_empty 
 }});
+
+list(1, 2, 3) //?
