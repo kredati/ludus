3edfe685bf1140a9493293c6edd5bc22462976d8
Scott Richmond <kredati@users.noreply.github.com>
2021-05-11 18:03:58 -0400

    Fix `raise`—it cannot be signed. Write better doc.

--- src/prelude/signed/errors.js
+++ src/prelude/signed/errors.js
@@ -7,20 +7,15 @@ import Spec from './spec.js';
 import NS from './ns.js';
 
 let {defn} = Fn;
-let {args, defspec, string, function: fn} = Spec;
+let {args, function: fn} = Spec;
 
-let error = defspec({name: 'error', pred: (x) => x instanceof Error});
-
-let raise = defn({
-  name: 'raise',
-  doc: 'Raises an error: stops execution and reports an error.',
-  pre: args([error, string]),
-  body: L.Err.raise
-});
+// We can't wrap `raise` in a `defn`, because it won't throw errors properly (they gety `handle`d)
+// Add `doc` metadata to raise.
+L.Err.raise.doc = 'Raises an error: stops execution and sends a report. You may raise a simple message or a specific error. `raise("foo");` will report out "foo" to the console as an error. `raise(Error, "foo");` will also report out a stack trace (which may or may not be helpful). You may use any type of Error. (Indeed, you may use anything as the first argument to `raise` and the console will do its best to represent it as a message.) Any additional arguments will also be reported out to the console on their own lines.';
 
 let bound = defn({
   name: 'bound',
-  doc: 'Converts a function that throws or raises an error to one that returns any errors.',
+  doc: 'Converts a function that raises an error to one that returns any errors.',
   pre: args([fn]),
   body: (fn) => Fn.fn(fn.name + '<bounded>', 
     (...args) => {
@@ -36,5 +31,5 @@ let bound = defn({
 // TODO: do we really need `handle`? All `fn` and `defn` functions are handled. No user should ever have to call this.
 
 export default NS.defns({name: 'Error',
-  members: {raise, bound}
+  members: {raise: L.Err.raise, bound}
 });
