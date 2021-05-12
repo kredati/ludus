adcc37b07305e91ace52b69c0b3f7ce231f83c44
Scott Richmond <kredati@users.noreply.github.com>
2021-05-11 20:45:46 -0400

    Very first draft of `defmethod`

---
+++ src/prelude/signed/method.js
@@ -0,0 +1,28 @@
+//////////////////// Methods
+// Methods are functions that dispatch to other functions--with the same name, in other namespaces. 
+// Methods are how we achieve polymorphism in Ludus. For example, many different types implement
+// a `conj` function: lists, arrays, strings, etc. A `conj` method lets us call the correct `conj`
+// for a datatype.
+
+import Arr from './arr.js';
+import List from './list.js';
+import Str from './strings.js';
+import Obj from './objs.js';
+import Seq from './seqs.js';
+import NS from './ns.js';
+import Fn from './fns.js';
+import ns from './ns.js';
+
+let {defn} = Fn;
+
+let defmethod = defn({
+    name: 'defmethod',
+    doc: 'Defines a method.',
+    body: ({name, doc}) => defn({
+        name, doc, 
+        body: (first, ...rest) => NS.get_ns(first)[name](first, ...rest)
+    })
+});
+
+let conj = defmethod({name: 'conj'}); //?
+conj([1, 2], 3, 4, 5, 6, 7, 8, 9); //?
