94c95577209e94d25d91bb5a55527bacdb6ec869
Scott Richmond <kredati@users.noreply.github.com>
2021-05-20 18:40:35 -0400

    Hammer out first draft of refs.

--- src/prelude/signed/refs.js
+++ src/prelude/signed/refs.js
@@ -2,66 +2,143 @@
 // Refs are the way Ludus handles state, which is also to say persistence,
 // which is also to say asynchronicity. They are modeled on Clojure's Atoms,
 // but Ludus calls "atoms" simple datatypes (e.g., strings, numbers, bools).
+// Ludus does not need the robust set of state-management constructs as
+// Clojure does, since JS is (mostly) single-threaded---and anyway, multi-
+// threading falls beyond Ludus's intended use cases. But we *do* need 
+// something.
 
-import * as env from './environment.js';
+// A ref contains a value, which can change over time. The way to change the
+// value of a ref is to pass it and a new value to `swap`. `swap` updates the
+// value of the ref immediately. In addition, you can add `Watcher`s to the
+// ref, which will call a function (presumably with side effects) when the
+// ref's value changs---once the event loop clears.
 
-// a cute helper I keep using
-// TODO: put this somewhere
-let create = (proto, attrs) => Object.assign(Object.create(proto), attrs);
+// In additon, to get refs working, we need a way to represent a deferred 
+// computation, that runs once the event loop clears; we use `future`.
+// It may be necessary to develop more robust constructs later (monads!) in
+// order to reify deferred computations (so that, e.g., they can be canceled).
+// But for now, this should do what we need.
 
-// a tag to mark references as different from everything else
-let ref_tag = Symbol('ludus/ref');
+import L from './deps.js';
+import T from './type.js';
+import NS from './ns.js';
+import Fn from './fns.js';
+import S from './spec.js';
 
-// a Ref prototype object
-let Ref = {
-  name: 'Ref',
-  [ref_tag] () { return this.constructor; },
-  get constructor () { return Ref; },
-  [globalThis[Symbol.for('ludus/inspect/custom')]] () {
-    let inspect = globalThis[Symbol.for('ludus/inspect')];
-    return `Ref${this.name ? ': ' + this.name : '' } { ${inspect(this.value)} }`;
+let {eq} = L;
+let {defn} = Fn;
+let {deftype, create} = T;
+let {record, maybe, string, any, args, type} = S;
+
+let Ref = deftype({name: 'Ref'});
+
+let ref_descriptor = record('ref_descriptor', {
+  name: maybe(string), 
+  value: any, 
+  doc: maybe(string)
+});
+
+let ref = defn({
+  name: 'ref',
+  doc: 'Creates a ref',
+  pre: args([ref_descriptor]),
+  body: ({name, doc, value, ...attrs}) => 
+    create(Ref, {name, doc, value, pending: false, watchers: [], attrs})
+});
+
+let show = defn({
+  name: 'show',
+  doc: 'Shows a ref.',
+  pre: args([type(Ref)]),
+  body: (ref) => `Ref${ref.name ? ': ' + ref.name : ''} { ${ref.value} }`
+});
+
+
+let deref = defn({
+  name: 'deref',
+  doc: 'Gets the value stored in a ref.',
+  pre: args([type(Ref)]),
+  body: ({value}) => value
+});
+
+let swap = defn({
+  name: 'swap',
+  doc: 'Updates the value in a ref, mutating its state. Returns the ref.',
+  pre: args([type(Ref), any]),
+  body: (ref, new_value) => {
+    if (!eq(ref.value, new_value)) {
+      ref.value = new_value;
+      ref.pending = true;
+      future(ping_watchers, [ref]);
+    };
+    return undefined;
   }
-};
+});
 
-let ref = ({name, value, ...attrs}) => 
-  create(Ref, {name, value, watchers: [], attrs});
-
-let deref = ({value}) => value;
-
-let swap = (ref, new_value) => {
-  ref.value = new_value;
-  future(ping_watchers, [ref]);
-  return ref; // is this the right return value?
-};
-
+// not exported
 let ping_watchers = (ref) => {
-  ref.watchers.forEach(([fn, args]) => {
-    fn(deref(ref), ...args); // TODO: debounce this?
-  });
-  return undefined;
-};
-
-let watch = (ref, fn, ...args) => {
-  ref.watchers.push([fn, args]);
-  future(fn, [deref(ref), ...args]); // TODO: don't call the watcher right away
-  return ref; // is this the right return value?
-};
-
-let unwatch = (ref, fn) => {
-  let {watchers} = ref;
-  let new_watchers = [];
-  for (let i = 0; i < watchers.length; i++) {
-    let [watcher] = watchers[i];
-    if (watcher === fn) continue;
-    new_watchers.push(watchers[i]);
+  if (ref.pending) {
+    for (let {fn, args} of ref.watchers) {
+      fn(deref(ref), ...args);
+    }
   }
-  ref.watchers = new_watchers;
-  return ref;
-};
-
-let future = (fn, args = [], timeout = 0) => {
-  setTimeout(() => { fn(...args); }, timeout);
+  ref.pending = false;
   return undefined;
 };
 
-export {ref, deref, swap, watch, unwatch, future};
+let Watcher = deftype({name: 'Watcher'});
+
+let watcher = defn({
+  name: 'watcher',
+  doc: 'Creates a watcher.',
+  body: (ref, fn, args) => create(Watcher, {ref, fn, args})
+});
+
+let watch = defn({
+  name: 'watch',
+  doc: 'Adds a watcher to a ref. The function (presumably with side effects) will be called whenever the ref changes value (i.e. somebody, somewhere `swap`ped the ref). It will call `fn` with any additional arguments passed to `watch`. Returns a Watcher, which you can then pass to `unwatch` to cancel the watcher.',
+  pre: args([type(Ref), S.function]),
+  body: (ref, fn, ...args) => {
+    let w = watcher(ref, fn, args);
+    ref.watchers.push(w);
+    return w;
+  }
+});
+
+let unwatch = defn({
+  name: 'unwatch',
+  doc: 'Removes a watcher from a ref, such that it will no longer be called when the ref\'s value changes.',
+  pre: args([type(Watcher)]),
+  body: (watcher) => {
+    let new_watchers = [];
+    let {ref} = watcher;
+    for (let w of ref.watchers) {
+      if (w !== watcher) {
+        new_watchers.push(w);
+      }
+    }
+    ref.watchers = new_watchers;
+    return undefined;
+  }
+});
+
+
+let future = defn({
+  name: 'future',
+  doc: 'Schedules a function call in the future.',
+  pre: args(
+    [S.function], 
+    [S.function, S.array], 
+    [S.function, S.array, S.integer]),
+  body: [
+    (fn) => future(fn, [], 0),
+    (fn, args) => future(fn, args, 0),
+    (fn, args, delay) => setTimeout(() => { fn(...args); }, delay)
+  ]
+});
+
+export default NS.defns({type: Ref,
+  members: {
+    Ref, ref, swap, watch, unwatch, future, show
+  }
+});
