e280971b9cd58c1f1cebbb26e91d5686c68355a6
Scott Richmond <kredati@users.noreply.github.com>
2021-05-25 11:15:11 -0400

    Add `eq` method under the hood; optimize `eq_assoc`

--- src/prelude/unsigned/eq.js
+++ src/prelude/unsigned/eq.js
@@ -1,13 +1,39 @@
+//////////////////// eq
+// An algorithm for more intuitive, value-based equality
+
+// Because of Ludus's approach to state and purity, it makes a great
+// deal more sense for equality to be value-based rather than
+// reference-based. We want `eq([1, 2, 3], [1, 2, 3]); //=> true`, but
+// `[1, 2, 3] === [1, 2, 3]; //=> false`. Same for objects.
+
+// At current, the algorithm below only does value-based equality for
+// object literals and anything that has an iterator, everything else
+// that isn't an atom is compared using reference-based equality.
+// We probably want to allow for user-defined behavior?--an `eq` method?
+
+// TODO:
+// [ ] dispatch to `eq` method if it exists, maybe?
+// [ ] what is the default behavior for Ludus types?
+// [ ] what to do when you've got two infinite sequences?
+//     ^ at current it will hang
+// [ ] optimize `eq_assoc` if possible
+
 import Pred from './preds.js';
+import L from './base.js';
 
 let {is_sequence, is_assoc, is_some} = Pred;
+
+let eq_ = L.defmethod({
+  name: 'eq',
+  not_found: () => false
+});
 
 let eq = (x, y) => {
   if (x === y) return true;
   if (x == undefined) return y == undefined;
-  if (is_sequence(x)) return eq_iter(x, y);
-  if (is_assoc(x)) return eq_assoc(x, y);
-  return false;
+  if (is_sequence(x) && is_sequence(y)) return eq_iter(x, y);
+  if (is_assoc(x) && is_assoc(y)) return eq_assoc(x, y);
+  return eq_(x, y);
 };
 
 // eq_iter is a helper function for `eq`
@@ -43,16 +69,12 @@ let eq_iter = (xs, ys) => {
 
 // `eq_assoc` tests for equality between associative collections.
 // Runs in O(1) best case, and O(😱) in worst case
-// Returns false if two objects do not have the same constructor
-// or prototype.
 // TODO: are there more clever optimizations?
 let eq_assoc = (x, y) => {
-  if (x.constructor !== y.constructor) return false;
-  if (Reflect.getPrototypeOf(x) !== y.getPrototypeOf(y)) return false;
-  for (let key in x) {
+  let x_keys = Object.keys(x);
+  for (let key in x_keys) {
     if (!eq(x[key], y[key])) return false;
   }
-  let x_keys = Object.keys(x);
   let y_keys = Object.keys(y);
   if (x_keys.length !== y_keys.length) return false;
 
