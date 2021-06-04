9e18389041069575504a4076df80c19e8e5179bb
Scott Richmond <kredati@users.noreply.github.com>
2021-06-03 20:59:29 -0400

    defn -> fn, add show

--- src/prelude/signed/objs.js
+++ src/prelude/signed/objs.js
@@ -8,12 +8,12 @@ import NS from './ns.js';
 import Spec from './spec.js';
 import Pred from './preds.js';
 
-let {defn, partial} = Fn;
+let {fn, partial} = Fn;
 let {is_any, is_key, is_obj, is_fn, is_js_obj} = Pred;
 let {args, dict, tup, iter_of} = Spec;
 let {ns} = NS;
 
-let get = defn({
+let get = fn({
   name: 'get',
   doc: `Gets the value stored at a particular key in an object. Returns \`undefined\` if value is not found. It returns \`undefined\` when looking for a property on anything that cannot have properties: e.g., \`undefined\`, booleans, and numbers. Given an indexed value (arrays, but also strings) it returns the value at the corresponding index. Returns only own properties. To get properties in the prototype chain, or at symbol keys, use \`get_\`.`,
   pre: args([is_key], [is_key, is_any]),
@@ -32,7 +32,7 @@ let get = defn({
   ]
 });
 
-let get_ = defn({
+let get_ = fn({
   name: 'get_',
   doc: 'Gets the value stored at a particular key in an object, traversing the JS prototype chain, and also allowing symbols to serve as keys. Returns `undefined` if an object is missing a key, or cannot store keys.',
   body: [
@@ -44,7 +44,7 @@ let get_ = defn({
   ]
 });
 
-let get_in = defn({
+let get_in = fn({
   name: 'get_in',
   doc: 'Nested property access. Given a collection, take a path to a particular value represented by a sequence of keys. Returns `undefined` if there is nothing at that path. E.g. `get_in({a: [1, 2, 3]}, [\'a\', 1]); //=> 2`.',
   // TODO: partial application with one argument?
@@ -63,14 +63,14 @@ let get_in = defn({
 
 // TODO: get object update functions to work nicely on sequences
 // Honestly, these feel like they might want to be methods
-let _assoc = defn({
+let _assoc = fn({
   name: 'assoc',
   doc: 'Returns a new object with the value at that key.',
   pre: args([is_obj, is_key, is_any]),
   body: (obj, key, value) => ({...obj, [key]: value})
 });
 
-let _assoc_ = defn({
+let _assoc_ = fn({
   name: 'assoc_',
   doc: 'Mutates an object, associating the value with the key.',
   pre: args([is_js_obj, is_key, is_any]),
@@ -80,7 +80,7 @@ let _assoc_ = defn({
   }
 });
 
-let dissoc = defn({
+let dissoc = fn({
   name: 'dissoc',
   doc: 'Returns a new object with the specified key removed. The opposite of `assoc`. E.g., `dissoc({a: 1, b: 2}, \'b\'); //=> {a: 1}',
   pre: args([is_obj, is_key]),
@@ -94,7 +94,7 @@ let dissoc = defn({
   }
 });
 
-let dissoc_ = defn({
+let dissoc_ = fn({
   name: 'dissoc_',
   doc: 'Deletes a key on an object, mutating the object.',
   pre: args([is_js_obj, is_key]),
@@ -104,7 +104,7 @@ let dissoc_ = defn({
   }
 });
 
-let update = defn({
+let update = fn({
   name: 'update',
   doc: 'Updates a value in an object. Takes, at minimum, an object, a key, and a function, and returns a new object with the value at the key updated to be the result of passing it to the function. If the key does not exist, passes `undefined` as the old value. Any additional arguments to the function can be supplied. E.g., `update({a: 1}, \'a\', inc); //=> {a: 2}`.',
   pre: args([is_obj, is_key, is_fn],
@@ -125,7 +125,7 @@ let update_in = defn({
 });
 */
 
-let update_with = defn({
+let update_with = fn({
   name: 'update_with',
   doc: 'Updates multiple properties at once. Given a collection and a dictionary of functions, returns a new object with updated values that are the result of applying the matching function to the value in the collection. Passes `undefined` as the argument if the value is not found. The dictionary of update functions must be flat, and the functions must be unary. E.g., `update_with({foo: 42}, {foo: inc}); //=> {foo: 43}`.',
   pre: args([is_obj, dict(is_fn)]),
@@ -140,63 +140,72 @@ let update_with = defn({
 
 let kv = tup(is_key, is_any);
 
-let concat = defn({
+let concat = fn({
   name: 'concat',
   doc: 'Creates a new object, combining the object as the first argument with a sequence of key-value tuples. If the key-value pairs duplicate a key, silently overwrites the value; later pairs take precedence. E.g., `concat({a: 1}, [[\'b\', 2], [\'c\', 3]]; //=> {a: 1, b: 2, c: 3}`.',
   pre: args([is_obj, iter_of(kv)]),
   body: (obj, kvs) => Object.assign(obj, Object.fromEntries(kvs))
 });
 
-let merge = defn({
+let merge = fn({
   name: 'merge',
   doc: 'Creates a new object, combining the objects passed in. If objects duplicate a key, silently overwrites the value; later objects take precedence.E.g. `assign({a: 1, b: 2}, {b: 3, c: 4}); //=> {a: 1, b: 3, c: 4}`.',
   pre: args([is_obj]),
   body: (...objs) => Object.assign({}, ...objs)
 });
 
-let keys = defn({
+let keys = fn({
   name: 'keys',
   doc: 'Returns an array of an object\'s keys. Returns an empty array if the object has no properties.',
   body: (obj) => obj == undefined ? [] : Object.keys(obj)
 });
 
-let values = defn({
+let values = fn({
   name: 'values',
   doc: 'Returns an array of the values stored in an object. Returns an empty array if the object has no properties.',
   body: (obj) => obj == undefined ? [] : Object.values(obj)
 });
 
-let entries = defn({
+let entries = fn({
   name: 'entries',
   doc: 'Returns an array of `[key, value]` pairs for each property on an object. Returns an empty array if the object has no properties. E.g., `entries({a: 1, b: 2}); //=> [ [ \'a\', 1 ], [ \'b\', 2 ] ]`.',
   body: (obj) => obj == undefined ? [] : Object.entries(obj)
 });
 
-let empty = defn({
+let empty = fn({
   name: 'empty',
   doc: 'Returns an empty object',
   body: () => ({})
 });
 
-let conj = defn({
+let conj = fn({
   name: 'conj',
   doc: '`conj`oins a `[key, value]` tuple to an object.',
   pre: args([is_obj, tup(is_key, is_any)]),
   body: (obj, [key, value]) => _assoc(obj, key, value)
 });
 
-let conj_ = defn({
+let conj_ = fn({
   name: 'conj_',
   doc: '`conj`oins a `[key, value]` tuple to an object, mutating the object.',
   pre: args([is_obj, tup(is_key, is_any)]),
   body: (obj, [key, value]) => _assoc_(obj, key, value)
 });
 
-let from = defn({
+let from = fn({
   name: 'from',
   doc: 'Creates an object from an iterable containing `[key, value]` tuples.',
   pre: args([iter_of(kv)]),
   body: (entries) => Object.fromEntries(entries)
+});
+
+let show = fn({
+  name: 'show',
+  doc: 'Shows an object by naïvely dumping everything into a string.',
+  pre: args([is_obj]),
+  body: (obj) => `{${entries(obj)
+    .map(([k, v]) => k + ': ' + L.show(v))
+    .join(', ')}}`
 });
 
 export default ns(L.Obj, {
@@ -205,5 +214,5 @@ export default ns(L.Obj, {
   merge, keys, values, entries,
   empty, conj, conj_, from,
   assoc: _assoc, assoc_: _assoc_, 
-  dissoc, dissoc_
+  dissoc, dissoc_, show
 });
