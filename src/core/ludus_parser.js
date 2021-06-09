f8d4c3199ace76c4a926d699df13e94d38314713
Scott Richmond <kredati@users.noreply.github.com>
2021-06-09 19:25:27 -0400

    Getting close to a complete parser.

--- src/core/ludus_parser.js
+++ src/core/ludus_parser.js
@@ -1,13 +1,31 @@
+//////////////////// A Ludus parser, in Ludus
+// Parser combinators and the stdlib, we can parse Ludus.
+// It turns out it's reasonably easy
+
+// TODOs:
+// [ ] destructuring assignment in `let`s
+// [ ] destructuring assignment in params
+// [ ] `as` aliases in imports and exports
+// [ ] comments
+// [ ] factor out lists, brackets
+// [ ] move forward references elsewhere (Fn? Parse?)
+// [ ] fix namespace export
+// [-] build AST from parsers
+// [ ] add template strings
+// [ ] add `js` to skip parsing
+// [ ] start working on good errors
+
 import '../prelude/prelude.js';
 import Parse from './parse.js';
 
-let {string, label, any_of, and_then, parse_char, opt, many, digit, run,map_parser, char_in_range, satisfy, many1, or_else, between, whitespace, line_break, sep_by, print_result, lowercase, uppercase, sep_by1} = Parse;
+let {string, label, and_then, parse_char, opt, many, digit, run,map_parser, char_in_range, satisfy, many1, or_else, between, whitespace, line_break, sep_by, lowercase, uppercase, sep_by1, keep_first, keep_second} = Parse;
 
 // a handy helper for forward references
+// where to put this, and how to enforce it's got to be a fn
 let forward = (name) => {
   let f = ref({name, value: () => {}});
   let out = Fn.rename(name, (...args) => deref(f)(...args));
-  return [out, f]
+  return [out, f];
 };
 
 // forward references for recursive parsers
@@ -15,30 +33,44 @@ let [literal, lit_ref] = forward('literal');
 
 let [expression, exp_ref] = forward('expression');
 
+// two helpful whitespace parsers
 let ws = many(whitespace);
 
 let wsl = many(or_else(whitespace, line_break));
 
-let undef_p = string('undefined');
+////////// Atoms
 
-let true_p = string('true');
+///// undefined
+let undef_p = map_parser(
+  (_) => undefined,
+  string('undefined'));
 
-let false_p = string('false');
+///// booleans
+let true_p = map_parser(
+  (_) => true,
+  string('true'));
+
+let false_p = map_parser(
+  (_) => false,
+  string('false'));
 
 let bool_p = label('boolean', or_else(true_p, false_p));
 
+///// numbers
 let nonzero_digit = satisfy('nonzero digit', char_in_range('1', '9'));
 
 let zero = label('zero', parse_char('0'));
 
 let sign = parse_char('-');
 
+// integers
 let int_p = label('int', 
     and_then([
       opt(sign), 
       nonzero_digit, 
       many(digit)]));
 
+// floating point
 let float_p = label('float',
     and_then([
       opt(sign),
@@ -49,8 +81,10 @@ let float_p = label('float',
       parse_char('.'), 
       many1(digit)]));
 
+// Infinity
 let infinity_p = and_then(opt(sign), string('Infinity'));
 
+// exponentiated numbers
 let exp_p = and_then([
   or_else(int_p, float_p),
   parse_char('e'),
@@ -65,6 +99,8 @@ let num_p = map_parser(
   ),
   or_else([float_p, exp_p, infinity_p, int_p]));
 
+///// Strings
+// special escape pairs
 let escape_pairs = [
   ['\\\'', '\''],   // single quote
   ['\\\"', '\"'],   // double quote
@@ -78,13 +114,14 @@ let escape_pairs = [
   ['\\t', '\t']     // tab
 ];
 
+// turn this mapping into a parser
 let escape_p = or_else(
   into([], 
     map(([to_match, matched]) => map_parser((_) => matched, string(to_match))),
     escape_pairs));
 
 let unescaped_single = satisfy(
-  'unescaped char', 
+  'unescaped char: single quote', 
   not(or(eq('\\'), eq('\''), eq('\n'))));
 
 let single_q = between(
@@ -94,7 +131,7 @@ let single_q = between(
 );
 
 let unescaped_double = satisfy(
-  'unescaped char',
+  'unescaped char: double quote',
   not(or(eq('\"'), eq('\\'), eq('\n')))
 );
 
@@ -104,8 +141,18 @@ let double_q = between(
   many(or_else(unescaped_double, escape_p))
 );
 
-let str_p = or_else(single_q, double_q); // TODO: add template strings
+let str_p = label('string', 
+  map_parser(
+    pipe(flatten, Str.from),
+    or_else(single_q, double_q)));
 
+let atom = label('atom', map_parser(
+  (value) => ({type: 'atom', value}),
+  or_else([undef_p, bool_p, num_p, str_p])));
+
+////////// Collections
+
+// handy parsers for collections
 let comma_separator = and_then([
   opt(wsl),
   parse_char(','),
@@ -113,178 +160,264 @@ let comma_separator = and_then([
 
 let spread = string('...');
 
-let arr_p = label('array', 
-  between(
-    and_then(parse_char('['), wsl), 
-    and_then(wsl, parse_char(']')),
-    sep_by(comma_separator, expression)));
+let trailing_comma = opt(and_then(ws, parse_char(',')))
 
+///// Identifiers & namespaces
+
+// identifiers are valid variable names
 let underscore = parse_char('_');
 
 let dollar = parse_char('$');
 
+// identifiers may not begin with capital letters or digits...
 let id_init = or_else([lowercase, underscore, dollar]);
-
+//... but they may include them later on
 let id_rest = or_else([lowercase, uppercase, underscore, dollar, digit]);
 
 let identifier = label('identifier', 
-  map_parser(flatten, and_then(id_init, many(id_rest))));
+  map_parser(
+    pipe(flatten, Str.from, (name) => ({type: 'identifier', value: name})), 
+    and_then(id_init, many(id_rest))));
 
-let colon_assignment = and_then([
-  wsl, parse_char(':'), wsl
-]);
-
-let key_value = label('key_value', and_then([
-  or_else(identifier, str_p), colon_assignment, expression
-]));
-
-let splat = label('splat', and_then(spread, identifier));
-
-let obj_p = label('object',
-  between(
-    and_then(parse_char('{'), opt(wsl)),
-    and_then(opt(wsl), parse_char('}')),
-    sep_by(comma_separator, or_else(key_value, splat))
-  ));
-
-swap(lit_ref, label('literal', or_else([
-  undef_p, bool_p, num_p, arr_p, obj_p, str_p
-])));
-
-let arrow = and_then([wsl, string('=>'), wsl]);
-
-let arg_assignment = identifier;
-
-let fn_params = between(
-  parse_char('('),
-  parse_char(')'),
-  sep_by(comma_separator, arg_assignment)
-);
-
-let [block, block_ref] = forward('block');
-
-let sem = parse_char(';');
-
-let fn_body = or_else(block, expression);
-
-let fn_def = and_then([fn_params, arrow, fn_body]);
-
+// namespaces, meanwhile, begin with a capital letter
 let ns_name = and_then([
   uppercase,
   many(id_rest)
 ]);
 
+// and they allow dot-access
 let ns_dot_id = and_then([
   many1(and_then(ns_name, parse_char('.'))),
   identifier
 ]);
 
+///// Arrays
+let arr_p = label('array', map_parser(
+  (result) => when(result) 
+    ? {type: 'array', value: [...result]} 
+    : {type: 'array', value: []},
+  between(
+    and_then(parse_char('['), wsl), 
+    and_then(wsl, parse_char(']')),
+    keep_first(
+      sep_by(comma_separator, expression),
+      trailing_comma)
+    )));
+
+////////// Objects
+// a few helpful constituents
+let colon_assignment = and_then([
+  wsl, parse_char(':'), wsl
+]);
+
+let key_value = label('key_value', map_parser(
+  ([key, value]) => ({type: 'pair', value: [key, value]}),
+  and_then([
+    keep_first(or_else(identifier, map_parser(
+        (value) => ({type: 'atom', value}), str_p)), 
+      colon_assignment), 
+    expression
+])));
+
+let splat = label('splat', map_parser(
+  (value) => ({type: 'splat', value}),
+  keep_second(spread, identifier)));
+
+let obj_p = label('object', map_parser(
+  (value) => ({type: 'object', value}),
+  between(
+    and_then(parse_char('{'), opt(wsl)),
+    and_then(opt(wsl), parse_char('}')),
+    keep_first(
+      sep_by(comma_separator, or_else([key_value, identifier, splat])),
+      trailing_comma)
+  )));
+
+////////// Finally, a literal parser
+// We talk about "function literals," but we defer them
+// to their own section
+swap(lit_ref, label('literal', or_else([
+  atom, arr_p, obj_p
+])));
+
+////////// Parens around expressions
+// We will need this for function invocation:
+// any expression wrapped in parens will just evaluate
+// to itself.
 let paren_exp = between(
-  and_then(parse_char('('), ws),
-  and_then(ws, parse_char(')')),
+  and_then(parse_char('('), wsl),
+  and_then(wsl, parse_char(')')),
   expression
 );
 
+////////// Functions
+
+///// Function definition
+// a cute little arrow
+let arrow = and_then([ws, string('=>'), wsl]);
+
+// this will get more complex
+// TODO: 
+// [ ] array destructuring
+// [ ] object destructuring
+// [ ] rest arguments 
+let arg_assignment = identifier;
+
+// function parameters
+let fn_params = label('params', map_parser(
+  (value) => when(value) 
+    ? {type: 'params', value}
+    : {type: 'params', value: []},
+  between(
+    parse_char('('),
+    parse_char(')'),
+    keep_first(
+      sep_by(comma_separator, arg_assignment),
+      trailing_comma))));
+
+// forward reference for a block, which requires statements
+let [block, block_ref] = forward('block');
+
+let fn_body = label('fn body', map_parser(
+  (value) => ({type: 'fn_body', value}),
+  or_else(block, expression)));
+
+let fn_def = label('function definition',
+  map_parser(
+    ([params, body]) => ({type: 'function', value: {params, body}}),
+    and_then(keep_first(fn_params, arrow), fn_body)));
+
+///// Function invocation
 let callable = or_else([
   identifier,
   ns_dot_id,
   paren_exp
 ]);
 
-let fn_call = and_then([
-  callable,
-  ws,
-  between(
-    and_then(parse_char('('), wsl),
-    and_then(wsl, parse_char(')')),
-    sep_by(comma_separator, expression)
-  )
-]);
+let when_undef = (x, default_value) => when(is_undef(x)) ? default_value : x;
 
-let when_exp = and_then([
-  string('when'),
-  ws,
-  paren_exp,
-  wsl,
-  parse_char('?'),
-  wsl,
-  expression,
-  wsl,
-  parse_char(':'),
-  wsl,
-  expression
-]);
+let fn_call = label('function call', map_parser(
+  ([called, args]) => ({type: 'call', 
+    value: {called, args: when_undef(args, [])}}),
+  and_then(
+    keep_first(callable, ws),
+    between(
+      and_then(parse_char('('), wsl),
+      and_then(wsl, parse_char(')')),
+      sep_by(comma_separator, expression)))));
 
-swap(exp_ref, label('expression', or_else([
-  paren_exp, literal, when_exp, fn_call, identifier, ns_dot_id, fn_def
+////////// Special forms
+
+///// when
+// when is Ludus's conditional form
+// it must be followed by a ternary expression
+// ternary expressions can only follow when
+let when_exp = label('when expression', map_parser(
+  ([[condition, if_true], if_false]) => ({
+    type: 'when',
+    value: {condition, if_true, if_false}
+  }),
+  and_then([
+    keep_second(
+      and_then(string('when'), ws), 
+      paren_exp), // condition expression
+    keep_second(
+      and_then([wsl, parse_char('?'), wsl]), 
+      expression), // if true
+    keep_second(
+      and_then([wsl, parse_char(':'), wsl]), 
+      expression) // if false
 ])));
 
-let let_stm = and_then([
-  wsl,
-  string('let'),
-  many1(whitespace),
-  identifier,
-  ws,
-  parse_char('='),
-  wsl,
-  expression,
-  ws,
-  sem
-]);
+let js;
 
-let return_stm = and_then([
-  wsl,
-  string('return'),
-  many1(whitespace),
-  expression,
-  ws,
-  sem
-]);
+swap(exp_ref, label('expression', map_parser(
+  (value) => ({type: 'expression', value}),
+  or_else([
+    paren_exp, literal, when_exp, fn_call, identifier, ns_dot_id, fn_def
+]))));
 
-let expr_stm = and_then([
-  wsl,
-  expression,
-  ws,
-  sem
-]);
+////////// Statements
 
-let bare_import = and_then([
-  wsl,
-  string('import'),
-  many1(whitespace),
-  str_p,
-  ws,
-  sem
-]);
+// out statement terminator
+let sem = and_then(ws, parse_char(';'));
 
-let ns_import = and_then([
-  wsl,
-  string('import'),
-  many1(whitespace),
-  ns_name,
-  many1(whitespace),
-  string('from'),
-  many1(whitespace),
-  str_p,
-  ws,
-  sem
-]);
+///// Expression statement
+// an expression, plus a semicolon: 'foo';
+let expr_stm = label('expr stm', map_parser(
+  (value) => ({type: 'statement', value}),
+  between(wsl, sem, expression)
+));
+
+// let statement
+// let foo = bar;
+let let_stm = label('let stm', map_parser(
+  ([identifier, expression]) => 
+    ({type: 'let', value: {identifier, expression}}),
+  and_then([
+    keep_second(
+      and_then([wsl, string('let'), many1(whitespace)]),
+      identifier),
+    keep_second(
+      and_then([ws, parse_char('='), wsl]),
+      keep_first(expression, sem))
+])));
+
+// return statement
+// return add(1, 2);
+let return_stm = label('return', map_parser(
+  (value) => ({type: 'return', value}),
+  keep_second(
+    and_then([wsl, string('return'), many1(whitespace)]),
+  keep_first(expression, sem))));
+
+// now we have enough to describe a function block
+// zero or more let or expression statements,
+// followed by a single return statement
+swap(block_ref, label('function block', map_parser(
+  (value) => ({type: 'block', value: [...flatten(value)]}),
+  between(
+    and_then(parse_char('{'), wsl),
+    and_then(wsl, parse_char('}')),
+    and_then( // enforce ordering of let/expression, then return, statements
+      many(or_else(let_stm, expr_stm)),
+      opt(return_stm)
+    )))
+));
+
+////////// Imports and exports
+// TODO: finish cleaning these up
+// TODO: finish AST-ifying these
+let bare_import = label('bare import', map_parser(
+  (imported) => ({type: 'bare_import', value: {imported}}),
+  keep_second(
+    and_then([wsl, string('import'), many1(whitespace)]),
+    keep_first(str_p, sem))
+));
+
+let ns_import = label('ns import', map_parser(
+  ([ns_name, imported]) => 
+    ({type: 'ns_import', 
+      value: {
+        imported, 
+        ns_name: thread(ns_name, flatten, Str.from)}}),
+  and_then(
+    keep_second(
+      and_then([wsl, string('import'), many1(whitespace)]),
+      ns_name),
+    keep_second(
+      and_then([many1(whitespace), string('from'), many1(whitespace)]),
+      keep_first(str_p, sem)))));
 
 let imports = and_then([
-  wsl,
-  string('import'),
-  many1(whitespace),
+  wsl, string('import'), many1(whitespace),
   between(
     and_then(parse_char('{'), wsl),
     and_then(wsl, parse_char('}')),
     sep_by1(comma_separator, identifier)
   ),
-  many1(whitespace),
-  string('from'),
-  many1(whitespace),
-  str_p,
-  ws,
-  sem
+  many1(whitespace), string('from'), many1(whitespace),
+  str_p, sem
 ]);
 
 let import_stm = or_else([
@@ -322,15 +455,6 @@ let exports = and_then([
 ]);
 
 let export_stm = or_else(ns_export, exports);
-
-swap(block_ref, between(
-  and_then(parse_char('{'), wsl),
-  and_then(wsl, parse_char('}')),
-  and_then( // enforce ordering of let/expression, then return, statements
-    many(or_else(let_stm, expr_stm)),
-    opt(return_stm)
-  )
-));
 
 let ludus_file = and_then([
   many(import_stm), // TODO: enforce a Ludus import at the beginning?
