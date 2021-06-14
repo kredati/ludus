7d4e2beb0f2c7b7cc57aa3ade1cbaeded10ceb6b
Scott Richmond <kredati@users.noreply.github.com>
2021-06-14 19:19:23 -0400

    Add more node types

--- src/core/ludus_eval.js
+++ src/core/ludus_eval.js
@@ -1,3 +1,16 @@
+//////////////////// Ludus REPL Evaluator
+// A beast of a function to check IDs and do small transforms for one-statement
+// at a time REPL interaction.
+
+// TODO:
+// [ ] Complete handlers for all node types
+// [ ] Add line numbers to ID nodes where we can get errors
+// [ ] Devise transforms:
+//    [ ] Subsequent `let`s at REPL for same ID simply lose the `let`.
+//    [ ] `import`s become async functions
+//    ^ because we only do one statement at a time in the REPL, we don't have to worry about line numbers or the parser forwarding the code in the statement.
+
+
 import LP from './ludus_parser.js';
 import Parse from './parse.js';
 
@@ -6,19 +19,21 @@ let forbidden = ['quux', 'quuz', 'when'];
 let ctx = ['foo', 'bar'];
 
 let repl_ctx = {
-  higher: ['foo', 'bar'], // global
+  higher: ['foo', 'bar'], // globals
   current: [],
   toplevel: true
 };
 
+
+
 let input1 = `let quul = (foo) => {
-  quul;
-  let fool = 42;
-  let baz = 23;
   let bar = 12;
+  let baz = 13;
+  let forb = when(foo) ? bar : baz(foo);
+  return forb;
 };`;
 
-let input2 = `let quux = quux(42);`;
+let input2 = `let quul = quul(42);`;
 
 let parsed1 = Parse.run(LP.repl_line, input1);
 
@@ -28,7 +43,7 @@ let ast1 = get('result', parsed1);//?
 
 let errs1 = get('errors', parsed1); //?
 
-let ast2 = get('result', parsed2);
+let ast2 = get('result', parsed2); //?
 
 let errs2 = get('errors', parsed2);
 
@@ -36,14 +51,15 @@ let ctx_includes_id = (ctx, id) => includes(id, concat(get('higher', ctx), get('
 
 let check_name = fn('check_name', [
   (ctx) => partial(check_name, ctx),
-  ({higher, current}, name) => {
+  (ctx, name) => {
+    let {higher, current, toplevel} = ctx;
     let forbidden_err = when(includes(name, forbidden))
       ? raise(`Cannot use reserved word ${name} as identifier.`)
       : undefined;
     let shadow_warn = when(includes(name, higher))
       ? warn(`Shadowed name ${name}.`)
       : undefined;
-    let redefinition_err = when(and(not(get('toplevel', ctx)), includes(name, current)))
+    let redefinition_err = when(and(not(toplevel), includes(name, current)))
       ? raise(`Cannot redeclare identifier ${name}.`)
       : undefined;
     return name;
@@ -92,15 +108,15 @@ let handlers = {
 
   block: (ctx, block) => every(check_ids(ctx), block),
 
-  array: () => {},
+  array: (ctx, array) => every(check_ids(ctx), array),
 
-  object: () => {},
+  object: (ctx, object) => every(check_ids(ctx), object),
 
-  pair: () => {},
+  pair: (ctx, [_, expr]) => check_ids(ctx, expr),
 
-  splat: () => {},
+  splat: (ctx, splat) => check_ids(ctx, splat),
 
-  when: () => {},
+  when: (ctx, when) => every(check_ids(ctx), values(when)),
 
   ns_import: () => {},
 
@@ -121,4 +137,4 @@ let check_ids = fn(
 );  
 
 check_ids(repl_ctx, ast1); //?
-//check_ids(repl_ctx, ast2); //?
+check_ids(repl_ctx, ast2); //?
