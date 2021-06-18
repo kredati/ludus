7c8a777b1cc22b10f9dad3b87ddbf5e1c2e68808
Scott Richmond <kredati@users.noreply.github.com>
2021-06-18 19:55:20 -0400

    First draft of `doc`, which I need

---
+++ src/core/doc.js
@@ -0,0 +1,55 @@
+import '../prelude/prelude.js';
+
+let doc_method = (method) => {
+  let name = get('name', method);
+  let docstring = get('doc', method, '');
+  return `${name}::method\n${docstring}`;
+};
+
+let doc_ludus_fn = (fn) => {
+  let name = get('name', fn);
+  let docstring = get('doc', fn);
+  let arities = map(get('length'), get('clauses', fn));
+  let params = into({}, map(
+    (f) => [get('length', f), thread(f, str, split('=>'), first, trim)],
+    get('clauses', fn)));
+  let spec_raw = map(
+    get('members'),
+    get_in(fn, ['pre', 'members'], []));
+  let render_spec = pipe(
+    (x) => map(get('name'), x),
+    join_with(', '),
+    (x) => str('(', x, ')')
+  );
+  let pretty_specs = into({}, map((spec) => [count(spec), render_spec(spec)], spec_raw)); //?
+  let arities_and_specs = Str.from(map((arity) => {
+    let param_str = get(arity, params);
+    let spec_str = get(arity, pretty_specs);
+    return when(spec_str) ? str(param_str, '::', spec_str) : param_str;
+  }, arities), '\n');
+  return `${name}::function\n${arities_and_specs}\n${or(docstring, '')}`;
+};
+
+let doc_js_fn = (f) => {
+  let name = get('name', f, 'anon. fn');
+  let params = thread(f, str, split('=>'), first, trim);
+  return `${name}::function\n${params}`;
+};
+
+let doc_fn = (f) => cond(get('type', f),
+  [eq(fn),      () => doc_ludus_fn(f)],
+  [eq(method),  () => doc_method(f)],
+  [is_any,      () => doc_js_fn(f)]
+);
+
+let doc = fn({
+  name: 'doc',
+  doc: 'Shows the documentation for a ludus value.',
+  body: (x) => handle(
+    () => when(is_fn(x)) ? doc_fn(x) : get('doc', x, 'no doc'), 
+    just('no doc!'))
+});
+
+Ludus.globalize('doc', doc);
+
+export {doc};
